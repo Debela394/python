7e4949299b0fe16e79d885bae76c71c1a79f62e5
Debela394 <debela394@gmail.com>
2023-12-17 22:39:03 +0300

    Commit the first file

---
+++ Lesson 1/01programm.py
@@ -0,0 +1,12 @@
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+# is use for comment case in python
